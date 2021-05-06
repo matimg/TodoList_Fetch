d3c5d8ff8345b1582432f3f78278d67ed06bef24
Matías Gandini <matias.gandinimg@gmail.com>
2021-05-06 13:01:28 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,38 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [item, setItem] = useState("");
+	const [itemList, setItemList] = useState([]);
+	let addItem = e => {
+		e.preventDefault();
+		console.log("hola");
+	};
+	const items = itemList.map((element, id) => (
+		<li key={id} className="list-group-item it">
+			{element}
+		</li>
+	));
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<p className="text-secondary display-1">todos</p>
+			<div className="container w-50">
+				<form onSubmit={addItem}>
+					<ul className="list-group">
+						<input
+							type="text"
+							className="list-group-item"
+							onChange={() => setItem(event.target.value)}
+						/>
+						{itemList}
+						<p
+							className="list-group-item text-secondary d-flex float-left pl-3"
+							style={{ fontSize: "12px" }}>
+							{itemList.length} items left
+						</p>
+					</ul>
+				</form>
+			</div>
 		</div>
 	);
 }
